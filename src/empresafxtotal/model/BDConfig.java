55ff3da80d516e923b164083a7a0b71203a89cdf
Bárbara Carrijo <barbarahcarrijo@gmail.com>
2016-12-11 19:23:54 -0200

    BDConfig

--- src/empresafxtotal/model/BDConfig.java
+++ src/empresafxtotal/model/BDConfig.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package empresafxtotal.model;
 
 /**
  *
- * @author Matheus
+ * @author Barbara
  */
 public class BDConfig {
     
-   public static final String URL = "jdbc:postgresql://localhost:5432/empresa";
-   public static final  String USR = "postgres";
-   public static final  String PWD = "root";
-   public static final  String DRIVER = "org.postgresql.Driver";
+    public static final String URL = "jdbc:postgresql://localhost/empresa";
+    public static final String USR = "postgres";
+    public static final String PWD = "1001";
+    public static final String DRIVER = "org.postgresql.Driver";
     
 }
